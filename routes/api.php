d9902513075e398828442a75b448fa7a5439cae3
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-11-11 15:27:38 -0300

    fix: remove frontend only files from project

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+// use App\Http\Controllers\AssetController;
+// use Illuminate\Support\Facades\Route;
+
+// Route::apiResource('assets', AssetController::class);
+// Route::apiResource('assets', AssetController::class); -> vulns
+// Route::apiResource('assets', AssetController::class); -> devices
+// Route::apiResource('assets', AssetController::class); -> gateways
+// Route::apiResource('assets', AssetController::class); -> user ??
+// Route::apiResource('assets', AssetController::class); -> topology ??
