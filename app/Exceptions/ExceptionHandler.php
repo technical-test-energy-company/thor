3b8c4116b79547f57a08ada0971c5efa4ad8fd9d
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-11-13 01:49:33 -0300

    feat: add global exception handler to reduce response size

---
+++ app/Exceptions/ExceptionHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class ExceptionHandler
+{
+    private const EXCEPTION_MESSAGE = 'exception_message';
+
+    public static function handle(Exceptions $exceptions): void
+    {
+        $exceptions->render(function (Throwable $e, Request $request) {
+            $request->attributes->set(self::EXCEPTION_MESSAGE, $e->getMessage());
+        });
+
+        $exceptions->respond(function (Response $response) {
+            $request = request();
+            $message = $request->attributes->get(self::EXCEPTION_MESSAGE);
+
+            if (! is_null($message)) {
+                return response()->json(['message' => $message], $response->getStatusCode());
+            }
+
+            return $response;
+        });
+    }
+}
