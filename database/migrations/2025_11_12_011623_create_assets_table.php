564ce437951c34876f046a5815ad567eede21d23
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-11-12 23:59:14 -0300

    feat: add database migrations and seeding for assets

--- database/migrations/2025_11_12_011623_create_assets_table.php
+++ database/migrations/2025_11_12_011623_create_assets_table.php
@@ -1,27 +1,35 @@
 <?php
 
+use App\Enums\Asset\AssetDeviceType;
+use App\Enums\Asset\AssetRisk;
+use App\Enums\Asset\AssetStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assets');
+    }
+
     public function up(): void
     {
         Schema::create('assets', function (Blueprint $table) {
             $table->id();
+            $table->text('uid')->unique();
+            $table->string('name', length: 100);
+            $table->string('description');
+            $table->enum('device_type', AssetDeviceType::cases());
+            $table->ipAddress();
+            $table->string('location', length: 2);
+            $table->enum('status', AssetStatus::cases());
+            $table->string('supplier', length: 100);
+            $table->enum('risk', AssetRisk::cases());
+            $table->decimal('risk_score');
             $table->timestamps();
+            $table->softDeletes();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('assets');
     }
 };
