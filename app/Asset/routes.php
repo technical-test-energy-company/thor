d273370f5df2e668ede51c6f65c00a20555d5826
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-11-13 18:33:54 -0300

    refactor: break down routes to context namespace

---
+++ app/Asset/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Asset;
+
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('assets', AssetController::class);
--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,3 @@
 <?php
 
-use App\Asset\AssetController;
-use Illuminate\Support\Facades\Route;
-
-Route::apiResource('assets', AssetController::class);
-
-// Route::apiResources([
-//     'posts' => PostController::class,
-// ]);
-// Route::apiResource('posts', PostController::class)->readOnly();
-
-// use App\Http\Controllers\PhotoController;
-// use App\Http\Controllers\PostController;
-
-// Route::apiResources([
-//     'photos' => PhotoController::class,
-//     'posts' => PostController::class,
-// ]);
-
-// Route::apiResource('assets', AssetController::class)->except;
-// Route::apiResource('assets', AssetController::class); -> vulns
-// Route::apiResource('assets', AssetController::class); -> devices
-// Route::apiResource('assets', AssetController::class); -> gateways
-// Route::apiResource('assets', AssetController::class); -> user ??
-// Route::apiResource('assets', AssetController::class); -> topology ??
+require __DIR__.'/../app/Asset/routes.php';
