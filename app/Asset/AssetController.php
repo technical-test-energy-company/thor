d89dd8bc19d87048cc2c1660c2891bd7a19563a2
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-11-14 00:49:09 -0300

    feat: add asset crud api functionalities

--- app/Asset/AssetController.php
+++ app/Asset/AssetController.php
@@ -2,15 +2,54 @@
 
 namespace App\Asset;
 
-use Illuminate\Database\Eloquent\Collection;
+use App\Asset\Requests\StoreAssetRequest;
+use App\Asset\Requests\UpdateAssetRequest;
 use Infrastructure\Http\Controllers\Controller;
+use Infrastructure\Http\Requests\IndexRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class AssetController extends Controller
 {
-    // public function index(): string
-    public function index(): Collection
+    private $assetService;
+
+    public function __construct(AssetService $assetService)
     {
-        // return 'ok';
-        return Asset::all();
+        $this->assetService = $assetService;
+    }
+
+    public function index(IndexRequest $request): Response
+    {
+        $data = $request->validated();
+        $response = $this->assetService->index($data);
+
+        return response($response);
+    }
+
+    public function store(StoreAssetRequest $request): Response
+    {
+        $data = $request->validated();
+        $response = $this->assetService->store($data);
+
+        return response($response, Response::HTTP_CREATED);
+    }
+
+    public function show(Asset $asset): Response
+    {
+        return response($asset);
+    }
+
+    public function update(UpdateAssetRequest $request, Asset $asset): Response
+    {
+        $data = $request->validated();
+        $response = $this->assetService->update($data, $asset);
+
+        return response($response);
+    }
+
+    public function destroy(Asset $asset): Response
+    {
+        $this->assetService->destroy($asset);
+
+        return response()->noContent();
     }
 }
---
+++ app/Asset/AssetService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Asset;
+
+use Illuminate\Pagination\CursorPaginator;
+use Infrastructure\Constants\Constants;
+
+class AssetService
+{
+    public function index($data): CursorPaginator
+    {
+        $limit = $data['limit'];
+
+        return Asset::orderBy(Constants::ID)->cursorPaginate($limit);
+
+        return $response;
+    }
+
+    public function store($data): Asset
+    {
+        $asset = Asset::create($data);
+
+        return $asset;
+    }
+
+    public function update($data, Asset $asset): Asset
+    {
+        $asset->updateOrFail($data);
+
+        return $asset->fresh();
+    }
+
+    public function destroy(Asset $asset): void
+    {
+        $asset->deleteOrFail();
+    }
+}
