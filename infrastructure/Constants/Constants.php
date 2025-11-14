75ddabfacd9a7eb5a216ce877e618bdff9d470cf
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-11-14 00:32:43 -0300

    feat: add more global constants to prevent magic strings

--- infrastructure/Constants/Constants.php
+++ infrastructure/Constants/Constants.php
@@ -5,4 +5,10 @@ namespace Infrastructure\Constants;
 class Constants
 {
     public const ID = 'id';
+
+    public const CREATED_AT = 'created_at';
+
+    public const UPDATED_AT = 'updated_at';
+
+    public const DELETED_AT = 'deleted_at';
 }
