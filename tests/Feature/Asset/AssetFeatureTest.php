926f12ba80138ed79667a1b5710c3587ba638ee2
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-11-16 04:49:14 -0300

    feat: add feature test for asset class

---
+++ tests/Feature/Asset/AssetFeatureTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Asset;
+
+use App\Asset\Asset;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AssetFeatureTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private const BASE_ROUTE = '/api/assets/';
+
+    public function test_asset_feature_test_should_create_read_update_and_delete(): void
+    {
+        // index - check empty database
+        $response_index_1 = $this->get(self::BASE_ROUTE);
+        $response_index_1->assertJson([]);
+
+        // store - create first item
+        $item_store_1 = Asset::factory()->make(['uid' => 's7']);
+        $id_store_1 = $item_store_1->uid;
+        $response_store_1 = $this->post(self::BASE_ROUTE, $item_store_1->toArray());
+        $response_store_1->assertJson($item_store_1->toArray());
+
+        // show - retrieve first item
+        $response_show_1 = $this->get(self::BASE_ROUTE.$id_store_1);
+        $response_show_1->assertJson($item_store_1->toArray());
+
+        // store - create second item
+        $item_store_2 = Asset::factory()->make(['uid' => 's8']);
+        $id_store_2 = $item_store_2->uid;
+        $response_store_2 = $this->post(self::BASE_ROUTE, $item_store_2->toArray());
+        $response_store_2->assertJson($item_store_2->toArray());
+
+        // index - check both created items
+        $response_index_2 = $this->get(self::BASE_ROUTE);
+        $response_index_2->assertJsonCount(2, 'data');
+
+        // update - change name of first item
+        $name_update_1 = 'Alpha';
+        $payload_update_1 = ['name' => $name_update_1];
+        $response_update_1 = $this->put(self::BASE_ROUTE.$id_store_1, $payload_update_1);
+        $response_update_1->assertJsonFragment($payload_update_1);
+
+        // show - retrieve updated first item
+        $response_show_2 = $this->get(self::BASE_ROUTE.$id_store_1);
+        $response_show_2->assertJsonFragment($payload_update_1);
+
+        // delete - delete second item
+        $response_destroy_1 = $this->delete(self::BASE_ROUTE.$id_store_2);
+        $response_destroy_1->assertNoContent();
+
+        // index - check database with only one item
+        $response_index_3 = $this->get(self::BASE_ROUTE);
+        $response_index_3->assertJsonCount(1, 'data');
+    }
+}
