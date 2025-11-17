d0104c514a3983c30fec3b9b85844d55be6b41e1
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-11-17 01:54:54 -0300

    feat: add vulnerability endpoints

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ require __DIR__.'/../app/Asset/routes.php';
 require __DIR__.'/../app/Device/routes.php';
 require __DIR__.'/../app/Gateway/routes.php';
 require __DIR__.'/../app/Topology/routes.php';
+require __DIR__.'/../app/Vulnerability/routes.php';
