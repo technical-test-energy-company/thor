eb922a0757250367fc62dbe3ac43b7576bda2c81
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-11-19 00:52:47 -0300

    chore: tune down to 25 the list per page

--- infrastructure/Http/Requests/IndexRequest.php
+++ infrastructure/Http/Requests/IndexRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class IndexRequest extends FormRequest
 {
-    public const DEFAULT_LIMIT = 100;
+    public const DEFAULT_LIMIT = 25;
 
     protected function prepareForValidation(): void
     {
